0d008aa3b261cb2ad4b6f7caf87f5ca25430b0ae
BH lab <bhkim003@naver.com>
2024-06-27 09:52:41 +0900

    cifar71나옴 epoch71만에

--- my_snn/modules/data_loader.py
+++ my_snn/modules/data_loader.py
@@ -26,6 +26,7 @@ from tqdm import tqdm
 
 from apex.parallel import DistributedDataParallel as DDP
 
+import random
 
 
 from modules.data_loader import *
@@ -79,6 +80,8 @@ def data_loader(which_data, data_path, rate_coding, BATCH, IMAGE_SIZE, ddp_on):
                                     batch_size =BATCH,
                                     shuffle = False,
                                     num_workers =2)
+        synapse_conv_in_channels = 1
+        
 
 
     if (which_data == 'CIFAR10'):
@@ -138,6 +141,8 @@ def data_loader(which_data, data_path, rate_coding, BATCH, IMAGE_SIZE, ddp_on):
                                     batch_size =BATCH,
                                     shuffle = False,
                                     num_workers =2)
+        synapse_conv_in_channels = 3
+        
 
 
         '''
@@ -188,15 +193,67 @@ def data_loader(which_data, data_path, rate_coding, BATCH, IMAGE_SIZE, ddp_on):
                                     batch_size =BATCH,
                                     shuffle = False,
                                     num_workers =2)
+        synapse_conv_in_channels = 1
 
-        
 
-    data_iter = IMAGE_PIXEL_CHANNEL = iter(train_loader)
-    images, labels = data_iter.next()
 
-    # 채널 수와 클래스 개수를 확인합니다.
-    synapse_conv_in_channels = images.shape[1]
+    # reference: H Zheng, Y Wu, L Deng, Y Hu, G Li. "Tdbn: Going Deeper with Directly-Trained Larger Spiking Neural Networks." Proceedings of the AAAI conference on artificial intelligence  (2021). Print.
+    if (which_data == 'DVS-CIFAR10'):
+        data_path = data_path + '/cifar-dvs'
+        train_path = data_path + '/train'
+        val_path = data_path + '/test'
+        train_dataset = DVSCifar10(root=train_path, transform=False, img_size = IMAGE_SIZE)
+        val_dataset = DVSCifar10(root=val_path, transform=False, img_size = IMAGE_SIZE)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH, shuffle=True,
+                                                    num_workers=2, pin_memory=True)
+        test_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH, shuffle=False,
+                                                    num_workers=2, pin_memory=True)
+        synapse_conv_in_channels = 2
+
+    # data_iter = iter(train_loader)
+    # images, labels = data_iter.next()
+
 
     return train_loader, test_loader, synapse_conv_in_channels
 
 
+
+# reference: H Zheng, Y Wu, L Deng, Y Hu, G Li. "Tdbn: Going Deeper with Directly-Trained Larger Spiking Neural Networks." Proceedings of the AAAI conference on artificial intelligence  (2021). Print.
+class DVSCifar10(Dataset):
+    def __init__(self, path, transform=None, img_size = 48):
+        self.path = path
+        self.transform = transform
+        self.img_size = img_size
+
+        # 일단 기본적인 resize는 무조건 해주고
+        # rotate하고 shear(x방향)하고 roll은 무작위로함.
+        self.resize = transforms.Resize(size=(self.img_size, self.img_size), interpolation=torchvision.transforms.InterpolationMode.NEAREST)
+        self.rotate = transforms.RandomRotation(degrees=30)
+        self.shearx = transforms.RandomAffine(degrees=0, shear=(-30, 30))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        data, target = torch.load(self.path + '/{}.pt'.format(index))
+        data = self.resize(data.permute([3, 0, 1, 2]))
+
+        if self.transform == True:
+            choices = ['roll', 'rotate', 'shear']
+            aug = np.random.choice(choices)
+            if aug == 'roll':
+                off1 = random.randint(-5, 5)
+                off2 = random.randint(-5, 5)
+                data = torch.roll(data, shifts=(off1, off2), dims=(2, 3))
+            if aug == 'rotate':
+                data = self.rotate(data)
+            if aug == 'shear':
+                data = self.shearx(data)
+
+        return data, target.long().squeeze(-1)
+
+    def __len__(self):
+        return len(os.listdir(self.path))
